4fdea915e2ba191f95df24cd5ca55b476b6dea7d
André Krüger <andre.kruger@consisa.com.br>
2025-02-25 15:16:07 -0300

    j

--- app/sound attr.js
+++ app/sound attr.js
@@ -1,17 +1,16 @@
-
 var SOUND_ATTRIBUTION = {
-    "spirits": "shadoWisp@freesound",
-    "blizzard":"DBlover@freesound",
-    "cave drone":"Kinoton@freesound",
-    "waterfall":"snds-good@freesound",
-    "fire ambience":"ahriik@freesound",
-    "mystical cavern ambience":"oneroidstate@freesound",
-    "wind from inside":"LolitaPerdurabo@freesound",
-    "wind":"FreqMan@freesound",
-    "gibbering whispers":"thanvannispen@freesound",
-    "ship at sea":"CGEffex@freesound",
-    "bubbling cauldron":"nomfundo@freesound",
-    "bubbling mud":"RTB45@freesound",
-    "wildlife ambience day":"InspectorJ (www.jshaw.co.uk)@freesound",
-    "beach ocean":"reklamacja@freesound"
-}
+    spirits: "shadoWisp@freesound",
+    blizzard: "DBlover@freesound",
+    "cave drone": "Kinoton@freesound",
+    waterfall: "snds-good@freesound",
+    "fire ambience": "ahriik@freesound",
+    "mystical cavern ambience": "oneroidstate@freesound",
+    "wind from inside": "LolitaPerdurabo@freesound",
+    wind: "FreqMan@freesound",
+    "gibbering whispers": "thanvannispen@freesound",
+    "ship at sea": "CGEffex@freesound",
+    "bubbling cauldron": "nomfundo@freesound",
+    "bubbling mud": "RTB45@freesound",
+    "wildlife ambience day": "InspectorJ (www.jshaw.co.uk)@freesound",
+    "beach ocean": "reklamacja@freesound",
+};
