8dd0d45a120e602ccca042e4218c816dd3fc1a54
André Krüger <andre.kruger@consisa.com.br>
2025-02-25 15:16:27 -0300

    l

--- app/about.js
+++ app/about.js
@@ -1,18 +1,14 @@
-
-var pathModule = require('path');
-const dataAccess = require("./js/dataaccess")
-const resourcePath = pathModule.join(window.api.getPath("userData"), 'data');
-const {ipcRenderer} = require("electron")
+var pathModule = require("path");
+const dataAccess = require("./js/dataaccess");
+const resourcePath = pathModule.join(window.api.getPath("userData"), "data");
+const { ipcRenderer } = require("electron");
 // assuming $ is jQuery
-$(document).on('click', 'a[href^="http"]', function (event) {
+$(document).on("click", 'a[href^="http"]', function (event) {
     event.preventDefault();
-    window.api.openBrowser(this.href)
-
+    window.api.openBrowser(this.href);
 });
 
-
-
-window.addEventListener("DOMContentLoaded", ()=>{
+window.addEventListener("DOMContentLoaded", () => {
     var modal = document.getElementById("myModal");
     createSoundAttribution();
     // Get the image and insert it inside the modal - use its "alt" text as a caption
@@ -20,57 +16,53 @@ window.addEventListener("DOMContentLoaded", ()=>{
     var img = document.getElementById("myImg");
     var modalImg = document.getElementById("img01");
     var captionText = document.getElementById("caption");
-    [...images].forEach(img => {
-        img.onclick = function(){
+    [...images].forEach((img) => {
+        img.onclick = function () {
             modal.style.display = "block";
             modalImg.src = this.src;
             captionText.innerHTML = this.alt;
-          }
+        };
     });
 
-
-    
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
-    
+
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() { 
-      modal.style.display = "none";
-    }
-    document.addEventListener("keydown",(e)=>{
-        if(e.keyCode == 27){
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+    document.addEventListener("keydown", (e) => {
+        if (e.keyCode == 27) {
             modal.style.display = "none";
         }
-    })
+    });
 });
 
 function setDrawer(index) {
     var drawers = [...document.getElementsByClassName("drawer_item")];
-    drawers.forEach(dr => dr.classList.remove("drawer_out"));
+    drawers.forEach((dr) => dr.classList.remove("drawer_out"));
 
     var sections = [...document.getElementsByClassName("content_section")];
-    sections.forEach(sec => sec.classList.add("hidden"));
+    sections.forEach((sec) => sec.classList.add("hidden"));
 
     drawers[index].classList.add("drawer_out");
-    sections[index].classList.remove("hidden")
-    $('#main_wrapper').animate({ scrollTop: 0 }, 0);
-
+    sections[index].classList.remove("hidden");
+    $("#main_wrapper").animate({ scrollTop: 0 }, 0);
 }
 
-function openDataFolder(){
-    window.api.openExplorer(resourcePath)
-    
+function openDataFolder() {
+    window.api.openExplorer(resourcePath);
 }
 
-function createSoundAttribution(){
+function createSoundAttribution() {
     var arr = Object.keys(SOUND_ATTRIBUTION);
     var valueArr = Object.values(SOUND_ATTRIBUTION);
     var res = [];
-    for(var i = 0 ; i < arr.length ; i++){
+    for (var i = 0; i < arr.length; i++) {
         var value = valueArr[i];
         var key = arr[i];
         res.push(`${key}: ${value}`);
     }
     var ele = document.getElementById("sound_attrubtion_container");
-    ele.innerHTML = `<h3>Attribution for sound effects used</h3>\n${res.join("<br>")}`
+    ele.innerHTML = `<h3>Attribution for sound effects used</h3>\n${res.join("<br>")}`;
 }
